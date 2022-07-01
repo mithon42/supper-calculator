54bdf1542d6e79229787f49f128b9ed238698051
mithon42 <mithonphilip@gmail.com>
2022-07-01 12:43:43 +0545

    Update v-1.1

--- js/app.js
+++ js/app.js
@@ -1,37 +1,4 @@
 
-const displayOutput = document.getElementById('display')
-
-const zero = document.getElementById('zero')
-const one = document.getElementById('one')
-const two = document.getElementById('two')
-const three = document.getElementById('three')
-const four = document.getElementById('four')
-const five = document.getElementById('five')
-const six = document.getElementById('six')
-const seven = document.getElementById('seven')
-const eight = document.getElementById('eight')
-const nine = document.getElementById('nine')
-
-const answer = document.getElementById('answer')
+let displayOutput = document.getElementById('result')
 
 
-
-function main(){
-    numberType(one)
-    numberType(two)
-    numberType(three)
-    numberType(four)
-    numberType(five)
-    numberType(six)
-    numberType(seven)
-    numberType(eight)
-    numberType(nine)
-}
-main()
-
-
-function numberType(typeBtn){
-    typeBtn.addEventListener('click', function(){
-        displayOutput.innerText = this.value
-    })
-}
