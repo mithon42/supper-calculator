9b91616cbd6ea443da75dc72006350ecb944128b
mithon42 <mithonphilip@gmail.com>
2022-07-01 13:25:35 +0545

    Calculator V-1.2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
+/**
+ * Author: Mithon Islam
+ * Dec: Awesome Mini Powerful Calculator.
+ * Date: 01-07-2022
+ * Day: Friday
+*/
 
-let displayOutput = document.getElementById('result')
+const sounds = new Audio('../Sounds/click-sounds.mp3');
+sounds.volume = 0.4;
+
+let result = document.getElementById('result')
 
 
+function numShow(number){
+    result.value += number;
+    sounds.play()
+}
+
+function clearAll(){
+    result.value = '';
+    sounds.play()
+}
+
+function del(){
+    result.value = result.value.slice(0, -1);
+    sounds.play()
+}
+
+function calculate(){
+    let x = result.value;
+    let y = eval(x);
+
+    sounds.play()
+    result.value = y;
+}
